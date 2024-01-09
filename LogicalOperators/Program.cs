b9d07ba0015d629d959fafe96a2bcf4ca991fc26
Nathan Strang <n.strang95@gmail.com>
2024-01-09 17:24:10 +0000

    Adding program code

---
+++ LogicalOperators/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LogicalOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Logical Operators can be used to check if more than one condition is true/false
+
+            Console.WriteLine("What's the temperature outside: (C)");
+            double temp = Convert.ToDouble(Console.ReadLine());
+
+            if (temp >= 10 && temp <= 25)           // An example of an AND operator 
+            {
+                Console.WriteLine("It's warm outside!");
+            }
+            else if (temp <= -50 || temp >=50)      // An example of an OR operator 
+            {
+                Console.WriteLine("Do not go outside!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
